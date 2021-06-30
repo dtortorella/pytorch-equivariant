5c0f26428696e9e00595348b48a99dfcefed447b
Domenico Tortorella <dom.tortorella@gmail.com>
2021-06-30 15:13:20 +0200

    Added equivariant linear function basis normalization

--- equivariant/functional.py
+++ equivariant/functional.py
@@ -17,23 +17,29 @@ def equivariant_1_to_0(x: Tensor) -> Tuple[Tensor, ...]:
     return e1,
 
 
-def equivariant_1_to_1(x: Tensor) -> Tuple[Tensor, ...]:
+def equivariant_1_to_1(x: Tensor, normalize: bool = False) -> Tuple[Tensor, ...]:
     """
     Equivariant linear basis for 1D -> 1D
 
     :param x: Input tensor of shape (B × D × N)
+    :param normalize: Normalize w.r.t. N
     :return: Output of the 2 linear basis functions, shape (B × D × N)
     """
     e1 = x
     e2 = x.sum(dim=-1, keepdim=True).expand_as(x)
-    return e1, e2
+    if normalize:
+        n = x.shape[-1]
+        return e1, e2/n
+    else:
+        return e1, e2
 
 
-def equivariant_1_to_2(x: Tensor) -> Tuple[Tensor, ...]:
+def equivariant_1_to_2(x: Tensor, normalize: bool = False) -> Tuple[Tensor, ...]:
     """
     Equivariant linear basis for 1D -> 2D
 
     :param x: Input tensor of shape (B × D × N)
+    :param normalize: Normalize w.r.t. N
     :return: Output of the 5 linear basis functions, shape (B × D × N × N)
     """
     n = x.shape[-1]
@@ -43,14 +49,18 @@ def equivariant_1_to_2(x: Tensor) -> Tuple[Tensor, ...]:
     e3 = x.unsqueeze(-1).expand(-1, -1, -1, n)
     e4 = e3.permute(0, 1, 3, 2)
     e5 = sum_all.unsqueeze(-1).expand(-1, -1, n, n)
-    return e1, e2, e3, e4, e5
+    if normalize:
+        return e1, e2/n, e3, e4, e5/n
+    else:
+        return e1, e2, e3, e4, e5
 
 
-def equivariant_2_to_1(x: Tensor) -> Tuple[Tensor, ...]:
+def equivariant_2_to_1(x: Tensor, normalize: bool = False) -> Tuple[Tensor, ...]:
     """
     Equivariant linear basis for 2D -> 1D
 
     :param x: Input tensor of shape (B × D × N × N)
+    :param normalize: Normalize w.r.t. N
     :return: Output of the 5 linear basis functions, shape (B × D × N)
     """
     n = x.shape[-1]
@@ -59,14 +69,18 @@ def equivariant_2_to_1(x: Tensor) -> Tuple[Tensor, ...]:
     e3 = x.sum(dim=-1)
     e4 = x.sum(dim=-2)
     e5 = e4.sum(dim=-1, keepdim=True).expand(-1, -1, n)
-    return e1, e2, e3, e4, e5
+    if normalize:
+        return e1, e2/n, e3/n, e4/n, e5/n**2
+    else:
+        return e1, e2, e3, e4, e5
 
 
-def equivariant_2_to_2(x: Tensor) -> Tuple[Tensor, ...]:
+def equivariant_2_to_2(x: Tensor, normalize: bool = False) -> Tuple[Tensor, ...]:
     """
     Equivariant linear basis for 2D -> 2D
 
     :param x: Input tensor of shape (B × D × N × N)
+    :param normalize: Normalize w.r.t. N
     :return: Output of the 15 linear basis functions, shape (B × D × N × N)
     """
     n = x.shape[-1]
@@ -90,14 +104,18 @@ def equivariant_2_to_2(x: Tensor) -> Tuple[Tensor, ...]:
     e13 = dia_sum.expand(-1, -1, n).diag_embed()
     e14 = dia.unsqueeze(-2).expand(-1, -1, n, -1)
     e15 = dia.unsqueeze(-1).expand(-1, -1, -1, n)
-    return e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15
+    if normalize:
+        return e1, e2/n, e3/n, e4/n, e5/n**2, e6/n, e7/n, e8/n, e9/n, e10, e11, e12, e13, e14/n, e15/n**2
+    else:
+        return e1, e2, e3, e4, e5, e6, e7, e8, e9, e10, e11, e12, e13, e14, e15
 
 
-def equivariant_2_to_2_symmetric(x: Tensor) -> Tuple[Tensor, ...]:
+def equivariant_2_to_2_symmetric(x: Tensor, normalize: bool = False) -> Tuple[Tensor, ...]:
     """
     Equivariant linear basis for 2D -> 2D, symmetric matrices
 
     :param x: Input tensor of shape (B × D × N × N)
+    :param normalize: Normalize w.r.t. N
     :return: Output of the 11 linear basis functions, shape (B × D × N × N)
     """
     n = x.shape[-1]
@@ -116,4 +134,7 @@ def equivariant_2_to_2_symmetric(x: Tensor) -> Tuple[Tensor, ...]:
     e13 = dia_sum.expand(-1, -1, n).diag_embed()
     e14 = dia.unsqueeze(-2).expand(-1, -1, n, -1)
     e15 = dia.unsqueeze(-1).expand(-1, -1, -1, n)
-    return e1, e3, e4, e5, e6, e10, e11, e12, e13, e14, e15
+    if normalize:
+        return e1, e3/n, e4/n, e5/n**2, e6/n, e10, e11, e12, e13, e14/n, e15/n**2
+    else:
+        return e1, e3, e4, e5, e6, e10, e11, e12, e13, e14, e15
